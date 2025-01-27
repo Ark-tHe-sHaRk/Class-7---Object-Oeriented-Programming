8acc43c3beaaeed10a2e5a7c19d96d38adffd709
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2025-01-27 16:54:20 +0000

    School Class

---
+++ School_Class.py
@@ -0,0 +1,9 @@
+#Creating a Class
+class Ark:
+    print('Hi! Welcome, your class name is Ark')
+class Dhyan:
+    print('Hi! Welcome, your class name is Dhyan')
+#Creating and Object
+
+ob = Ark()
+ob1 = Dhyan()
